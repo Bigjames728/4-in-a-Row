e33804f666d2a4ffebd697092b608e3d48b8e94a
Bigjames728 <bigjames728@gmail.com>
2020-12-07 15:09:31 -0800

    Update to Player class.
    Added the get method unusedTokens() and activeToken() to show the tokens that haven't dropped yet as well as the active token being used in play.

--- js/Player.js
+++ js/Player.js
@@ -24,5 +24,21 @@ class Player {
         return tokens;
     }
 
+    /**
+     * Gets all tokens that haven't been dropped.
+     * @return {array}   Array of unsused tokens.
+     */
 
+     get unusedTokens() {
+         return this.tokens.filter(token => !token.dropped);
+     }
+
+     /**
+      * Gets the active token by returning the first token in the array of unused tokens.
+      * @return {Object} First token object in the array of unused tokens.
+      */
+
+      get activeToken() {
+          return this.unusedToken[0];
+      }
 }
