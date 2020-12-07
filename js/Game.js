e196f132a10ff0e0432d5999bd342d0a74135056
Bigjames728 <bigjames728@gmail.com>
2020-12-07 14:20:29 -0800

    Added an empty method called startGame()

--- js/Game.js
+++ js/Game.js
@@ -15,4 +15,12 @@ class Game {
                          new Player('Player 2', 2, '#e59a13')];
         return players;
     }
+
+    /**
+     * Gets a game ready for play.
+     */
+
+    startGame() {
+
+    }
 }
