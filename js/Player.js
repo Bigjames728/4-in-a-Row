13aa025ef2a5517e07ba33d667a229434d19ab81
Bigjames728 <bigjames728@gmail.com>
2020-12-07 12:19:42 -0800

    Update to Player class
    Added player constructor method and added all of the properties.

--- js/Player.js
+++ js/Player.js
@@ -1,5 +1,11 @@
 class Player {
+    constructor(name, id, color, active) {
+        this.name = name;
+        this.id = id;
+        this.color = color;
+        this.active = active;
+        this.tokens = [];
+    }
 
-    
 
 }
