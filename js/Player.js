408a31602aea9f981a28de58fb5eac73e7499d1e
Bigjames728 <bigjames728@gmail.com>
2020-12-08 15:09:44 -0800

    Major update to Player.js.
    Added checkToken() method to see if there are any tokens left for a player. Also added activeToken() method.

--- js/Player.js
+++ js/Player.js
@@ -29,16 +29,25 @@ class Player {
      * @return {array}   Array of unsused tokens.
      */
 
-     get unusedTokens() {
+    get unusedTokens() {
          return this.tokens.filter(token => !token.dropped);
-     }
+    }
 
-     /**
-      * Gets the active token by returning the first token in the array of unused tokens.
-      * @return {Object} First token object in the array of unused tokens.
-      */
+    /**
+     * Gets the active token by returning the first token in the array of unused tokens.
+     * @return {Object} First token object in the array of unused tokens.
+     */
 
-      get activeToken() {
-          return this.unusedTokens[0];
-      }
+    get activeToken() {
+        return this.unusedTokens[0];
+    }
+
+    /**
+     * Check if a player has any undropped tokens left
+     * @return {Boolean}
+     */
+
+    checkTokens() {
+        return this.unusedTokens.length == 0 ? false : true;
+    }
 }
